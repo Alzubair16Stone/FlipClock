4189ba69a3dfeaf6012f47468e16501a7795ea4f
Alzubair <alzubair16stone@gmail.com>
2026-05-12 11:07:58 +0200

    exported interfaces, installed lucide-react

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,14 +1,15 @@
-interface backColor {
+export interface backColor {
     color: "#FFFFFF" | "#212529" | "#FFD700",
     background: "#1A1A1B" | "#F8F9FA" | "#001F3F"
 
 };
 
-interface changeColor {
+export interface changeColor {
     id: number,
     color: backColor,
 };
 
-interface setBgImg {
+export interface setBgImg {
     img: string
 };
+
